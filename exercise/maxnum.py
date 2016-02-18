6c247a2524eca5edbcf17b4b1de32b734f03ee80
Jasmine <Jasmine>
2016-02-18 12:20:49 +0100

    Removed files 2

--- exercise/maxnum.py
+++
@@ -1,6 +0,0 @@
-def max_num(num):
-Count = nums[i]
-for i in num :
-	if i > Count
-		Count = i
-	return count
--- exercise/minnum.py
+++
@@ -1,6 +0,0 @@
-def min_num(num):
-Count = nums[i]
-for i in num :
-	if i < Count
-		Count = i
-	return count
